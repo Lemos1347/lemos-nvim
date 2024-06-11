afe037aa5b2f698bd86f70fa07eefa304fbfac7b
Lemos1347 <henrique.matias@sou.inteli.edu.br>
2024-06-11 11:30:04 -0300

    feat: new plugin 'flash'

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -435,6 +435,20 @@ local default_plugins = {
       require("telescope").load_extension("lazygit")
     end,
   },
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {},
+  -- stylua: ignore
+  keys = {
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    { "<Esc>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+  },
+  },
 }
 
 local config = require("core.utils").load_config()
