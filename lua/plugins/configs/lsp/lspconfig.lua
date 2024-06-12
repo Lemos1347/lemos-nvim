988267921c25654fe3fc889f91d1197054a37288
Lemos1347 <henrique.matias@sou.inteli.edu.br>
2024-06-12 00:58:42 -0300

    feat: inlay hints for rust and deactivating rust tools

--- lua/plugins/configs/lsp/lspconfig.lua
+++ lua/plugins/configs/lsp/lspconfig.lua
@@ -162,84 +162,92 @@ lspconfig.pyright.setup({
 })
 
 -- configure rust analyzer with rust_tools
-local rust_tools_opts = {
-  tools = {
-    runnables = {
-      use_telescope = true,
-    },
-    inlay_hints = {
-      auto = true,
-      show_parameter_hints = false,
-      parameter_hints_prefix = "",
-      other_hints_prefix = "",
-    },
-  },
-
-  -- all the opts to send to nvim-lspconfig
-  -- these override the defaults set by rust-tools.nvim
-  -- see https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rust_analyzer
-  server = {
-    -- on_attach is a callback called when the language server attachs to the buffer
-    on_attach = M.on_attach,
-    settings = {
-      -- to enable rust-analyzer settings visit:
-      -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
-      ["rust-analyzer"] = {
-        completition = {
-          dynamicRegistration = true,
-        },
-        imports = {
-          granularity = {
-            group = "module",
-          },
-          prefix = "self",
-        },
-        cargo = {
-          buildScripts = {
-            enable = true,
-          },
-        },
-        procMacro = {
-          enable = true,
-        },
-        -- enable clippy on save
-        checkOnSave = {
-          command = "clippy",
-        },
-      },
-    },
-  },
-}
+-- local rust_tools_opts = {
+--   tools = {
+--     runnables = {
+--       use_telescope = true,
+--     },
+--     inlay_hints = {
+--       auto = true,
+--       show_parameter_hints = false,
+--       parameter_hints_prefix = "",
+--       other_hints_prefix = "",
+--     },
+--   },
 --
-require("rust-tools").setup(rust_tools_opts)
-
--- lspconfig.rust_analyzer.setup({
---   on_attach = M.on_attach,
---   settings = {
---     ["rust-analyzer"] = {
---       diagnostics = {
---         enable = true,
---       },
---       completition = {
---         dynamicRegistration = true,
---       },
---       imports = {
---         granularity = {
---           group = "module",
+--   -- all the opts to send to nvim-lspconfig
+--   -- these override the defaults set by rust-tools.nvim
+--   -- see https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rust_analyzer
+--   server = {
+--     -- on_attach is a callback called when the language server attachs to the buffer
+--     on_attach = M.on_attach,
+--     settings = {
+--       -- to enable rust-analyzer settings visit:
+--       -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
+--       ["rust-analyzer"] = {
+--         completition = {
+--           dynamicRegistration = true,
 --         },
---         prefix = "self",
---       },
---       cargo = {
---         buildScripts = {
+--         imports = {
+--           granularity = {
+--             group = "module",
+--           },
+--           prefix = "self",
+--         },
+--         cargo = {
+--           buildScripts = {
+--             enable = true,
+--           },
+--         },
+--         procMacro = {
 --           enable = true,
 --         },
---       },
---       procMacro = {
---         enable = true,
+--         -- enable clippy on save
+--         checkOnSave = {
+--           command = "clippy",
+--         },
 --       },
 --     },
 --   },
--- })
+-- }
+-- --
+-- require("rust-tools").setup(rust_tools_opts)
+
+lspconfig.rust_analyzer.setup({
+  on_attach = M.on_attach,
+  settings = {
+    ["rust-analyzer"] = {
+      diagnostics = {
+        enable = true,
+      },
+      completition = {
+        dynamicRegistration = true,
+      },
+      imports = {
+        granularity = {
+          group = "module",
+        },
+        prefix = "self",
+      },
+      cargo = {
+        buildScripts = {
+          enable = true,
+        },
+      },
+      procMacro = {
+        enable = true,
+      },
+      inlayHints = {
+        bindingModeHints = { enable = true },
+        closureCaptureHints = { enable = true },
+        closureReturnTypeHints = { enable = "always" },
+        lifetimeElisionHints = { enable = "always", useParameterNames = true },
+        rangeExclusiveHints = { enable = true },
+        typeHints = { hideClosureInitialization = true },
+      },
+    },
+  },
+})
 
 --configure golps
 lspconfig.gopls.setup({
